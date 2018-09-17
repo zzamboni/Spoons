e6eeb0ea0a21122d655727e7748be7def54aa2bd
Diego Zamboni <diego@zzamboni.org>
2018-09-17 22:51:40 +0200

    Various changes
    - Renamed methods to camelCase as dictated by Spoon convention
    - New method touchbarWidget to configure widgets that can run Lua code
      as part of their display update, and also when pressed.

--- Source/BetterTouchTool.spoon/init.lua
+++ Source/BetterTouchTool.spoon/init.lua
@@ -35,7 +35,7 @@ function obj:update_trigger(uuid, payload)
   return hs.osascript.applescript(code)
 end
 
---- BetterTouchTool:btticondata_from_hsimage(image)
+--- BetterTouchTool:hsimageToBTTIconData(image)
 --- Method
 --- Return the base64-encoded version of an hs.image object.
 ---
@@ -44,7 +44,7 @@ end
 ---
 --- Returns:
 ---  * A string containing the base64-encoded image.
-function obj:btticondata_from_hsimage(img)
+function obj:hsimageToBTTIconData(img)
   return img:encodeAsURLString():gsub("^data:image/png;base64,", "")
 end
 
@@ -54,7 +54,7 @@ function _1_255(num)
 end
 
 --- Convert a Hammerspoon hs.color object to a color string as used by BTT: "R, G, B, G" (each in 0-255 range, floating-point numbers)
-function _hscolor_to_rgb(color)
+function obj:hscolorToBTTColor(color)
   if type(color) == 'table' then
     local c = hs.drawing.color.asRGB(color)
     return string.format("%.6f, %.6f, %.6f, %.6f", _1_255(c.red), _1_255(c.green), _1_255(c.blue), _1_255(c.alpha))
@@ -63,7 +63,7 @@ function _hscolor_to_rgb(color)
   end
 end
 
---- BetterTouchTool:touchbar_button(uuid, spec)
+--- BetterTouchTool:touchbarButton(uuid, spec)
 --- Method
 --- Update a BTT TouchBar button definition
 ---
@@ -73,21 +73,22 @@ end
 ---    * uuid - UUID of the BTT object to update. Can also be passed in the first argument (takes precedence if specified in `spec`).
 ---    * name - string value for the `BTTTouchBarButtonName` field, which determines the string shown in the button. If set to an empty string, the button is hidden.
 ---    * icon - an `hs.image` object containing an icon to show in the button.
+---    * icon_size - an `hs.geometry` size object specifing the size to use for the icon.
+---    * icon_only - a boolean to specify if only the icon should be shown.
 ---    * color - an `hs.drawing.color` object specifying the background color for the button. Can also be a string already in BTT's "R, G, B, G" format.
 ---    * code - a string containing arbitrary Lua code to execute. The code will be executed using the `hs` command, so the whole Hammerspoon environment, loaded config, spoons, etc. are available.
----    * Any fields starting with "BTT" are passed unmodified to the underlying call to BTT's `update_trigger` API call.
+---    * Any fields starting with "BTT" are passed unmodified to the underlying call to BTT's `update_trigger` API call. If the value is a table, its contents is copied without further inspection (this can be used to populate the `BTTTriggerConfig` table common in some BTT widgets.
 ---
 --- Returns:
 ---  * The return values from [hs.osascript.applescript](http://www.hammerspoon.org/docs/hs.osascript.html#applescript) on the execution of the AppleScript code to the update_trigger BTT method.
-function obj:touchbar_button(uuid, spec)
+function obj:touchbarButton(uuid, spec)
   local payload = {
     BTTTouchBarButtonName = "",
     BTTTriggerType = 629,
     BTTTriggerClass = "BTTTriggerTypeTouchBar",
-    BTTPredefinedActionType = 206,
-    BTTPredefinedActionName = "Execute Shell Script / Task",
-    BTTShellTaskActionScript = "",
-    BTTShellTaskActionConfig = string.format("/usr/local/bin/hs:::-c:::Hammerspoon-configured: %s", spec.name or ""),
+    BTTPredefinedActionType = 137,
+    BTTPredefinedActionName = "Execute Terminal Command (Async, non-blocking)",
+    BTTTerminalCommand = "",
     BTTTriggerConfig = {}
   }
   for k,v in pairs(spec) do
@@ -96,13 +97,107 @@ function obj:touchbar_button(uuid, spec)
     elseif k == "name" then
       payload.BTTTouchBarButtonName = v
     elseif k == "icon" then
-      payload.BTTIconData = obj:btticondata_from_hsimage(v)
+      payload.BTTIconData = obj:hsimageToBTTIconData(v)
     elseif k == "code" then
-      payload.BTTShellTaskActionScript = v
+      payload.BTTTerminalCommand = string.format("/usr/local/bin/hs -c '%s'",v)
     elseif k == "color" then
-      payload.BTTTriggerConfig.BTTTouchBarButtonColor = _hscolor_to_rgb(v)
+      payload.BTTTriggerConfig.BTTTouchBarButtonColor = obj:hscolorToBTTColor(v)
+    elseif k == "icon_only" then
+      payload.BTTTriggerConfig.BTTTouchBarOnlyShowIcon = v
+    elseif k == "icon_size" then
+      payload.BTTTriggerConfig.BTTTouchBarItemIconWidth = v.w
+      payload.BTTTriggerConfig.BTTTouchBarItemIconHeight = v.h
     elseif string.match(k, "^BTT") then
-      payload[k] = v
+      if type(v) == "table" then
+        if not payload[k] then
+          payload[k] = {}
+        end
+        for k2,v2 in pairs(v) do
+          payload[k][k2] = v2
+        end
+      else
+        payload[k] = v
+      end
+    end
+  end
+  return obj:update_trigger(uuid, payload)
+end
+
+--- BetterTouchTool:touchbarWidget(uuid, spec)
+--- Method
+--- Update a BTT TouchBar widget definition. A widget has the ability to run a script and determine its icon, text and color from the output of the script. In addition, it can execute another script when touched.
+---
+--- Parameters:
+---  * uuid - UUID of the BTT object to update. Can be obtained from the BTT window by right-clicking on the desired object and choosing "Copy UUID". Can be `nil` if `spec` contains a `uuid` field.
+---  * spec - table containing the specification for the object. Can contain the following fields:
+---    * uuid - UUID of the BTT object to update. Can also be passed in the first argument (takes precedence if specified in `spec`).
+---    * name - string value for the `BTTWidgetName` fields, which determines the widget name.
+---    * icon - an `hs.image` object containing the initial icon to show in the button. Can be updated by the widget script output.
+---    * icon_size - an `hs.geometry` size object specifing the size to use for the icon.
+---    * icon_only - a boolean to specify if only the icon should be shown.
+---    * color - an `hs.drawing.color` object specifying the background color for the button. Can also be a string already in BTT's "R, G, B, G" format.
+---    * code - a string containing arbitrary Lua code to execute when the widget is clicked. The code will be executed using the `hs` command, so the whole Hammerspoon environment, loaded config, spoons, etc. are available.
+---    * widget_code - a string containing arbitrary Lua code to execute within the widget. The script you can either return a simple string which will then be shown on the widget (if it's an empty string, the widget is hidden), or you can return a JSON string containing any of the following fields (you can call `hs.json.encode` on a Lua table to produce this string):
+---      * text: string to show on the widget (if set to an empty string, the widget is hidden)
+---      * icon_data: a string containing a base64-encoded icon for the widget. You can use `BetterTouchTool:hsimageToBTTIconData` to produce this string from an `hs.image` object.
+---      * icon_path: a string containing the path of an image to use as the widget icon. If both `icon_data` and `icon_path` are specified, `icon_data` takes precedence.
+---      * background_color: a string in "R,G,B,Gamma" format specifying the background color for the widget. You can use `BetterTouchTool:hscolorToBTTColor` to produce this string from an `hs.drawing.color` object.
+---      * font_color: a string containing the font color for the widget, in the same format as background_color.
+---      * font_size: an integer specifying the font size for the widget text.
+---    * widget_interval - how often the widget should be updated, in seconds.
+---    * Any fields starting with "BTT" are passed unmodified to the underlying call to BTT's `update_trigger` API call. If the value is a table, its contents is copied without further inspection (this can be used to populate the `BTTTriggerConfig` table common in some BTT widgets.
+---
+--- Returns:
+---  * The return values from [hs.osascript.applescript](http://www.hammerspoon.org/docs/hs.osascript.html#applescript) on the execution of the AppleScript code to the update_trigger BTT method.
+function obj:touchbarWidget(uuid, spec)
+  local payload = {
+    BTTWidgetName = "",
+    BTTTriggerType = 642,
+    BTTTriggerTypeDescription = "Shell Script / Task Widget",
+    BTTPredefinedActionType = 137,
+    BTTPredefinedActionName = "Execute Terminal Command (Async, non-blocking)",
+    BTTTriggerClass = "BTTTriggerTypeTouchBar",
+    BTTTerminalCommand = "",
+    BTTShellScriptWidgetGestureConfig = "/usr/local/bin/hs:::-c",
+    BTTTriggerConfig = {
+      BTTScriptType = 0,
+      BTTTouchBarShellScriptString = "",
+      BTTTouchBarScriptUpdateInterval = 30,
+      BTTTouchBarButtonName = "",
+      BTTTouchBarOnlyShowIcon = false,
+    }
+  }
+  for k,v in pairs(spec) do
+    if k == "uuid" then
+      uuid = v
+    elseif k == "name" then
+      payload.BTTWidgetName = v
+    elseif k == "icon" then
+      payload.BTTIconData = obj:hsimageToBTTIconData(v)
+    elseif k == "code" then
+      payload.BTTTerminalCommand = string.format("/usr/local/bin/hs -c '%s'",v)
+    elseif k == "widget_code" then
+      payload.BTTTriggerConfig.BTTTouchBarShellScriptString = v
+    elseif k == "widget_interval" then
+      payload.BTTTriggerConfig.BTTTouchBarScriptUpdateInterval = v
+    elseif k == "color" then
+      payload.BTTTriggerConfig.BTTTouchBarButtonColor = obj:hscolorToBTTColor(v)
+    elseif k == "icon_only" then
+      payload.BTTTriggerConfig.BTTTouchBarOnlyShowIcon = v
+    elseif k == "icon_size" then
+      payload.BTTTriggerConfig.BTTTouchBarItemIconWidth = v.w
+      payload.BTTTriggerConfig.BTTTouchBarItemIconHeight = v.h
+    elseif string.match(k, "^BTT") then
+      if type(v) == "table" then
+        if not payload[k] then
+          payload[k] = {}
+        end
+        for k2,v2 in pairs(v) do
+          payload[k][k2] = v2
+        end
+      else
+        payload[k] = v
+      end
     end
   end
   return obj:update_trigger(uuid, payload)
@@ -117,23 +212,27 @@ end
 ---    * name - a string with the Spoon name. Must match the name by which the Spoon object can be accessed (`spoon.<name>`)
 ---    * spoon_action_mappings - a table containing a mapping from action names to functions to be called when the action is invoked. It's the same format expected by the `def` argument of the [`hs.spoons.bindHotkeysToSpec()](http://www.hammerspoon.org/docs/hs.spoons.html#bindHotkeysToSpec) function (ideally, the same object should be used for both, so that spoon actions can be bound to both hotkeys and BTT triggers).
 ---  * spec - a table containing the action name-to-BTT trigger mappings. Its keys must exist in the `spoon.spoon.action_mappings` table, and the value must be a table with the following fields:
----    * kind - a string containing the trigger type to bind. Must correspond to an existing method in this spoon. Currently only `touchbar_button` is supported, and uses `BetterTouchTool:touchbar_button` to configure the BTT trigger.
----    * Any fields supported by the underlying method call according to `kind`, e.g. `BetterTouchTool:touchbar_button`.
+---    * kind - a string containing the trigger type to bind. Must correspond to an existing method in this spoon. Currently only `touchbarButton` is supported, and uses `BetterTouchTool:touchbarButton` to configure the BTT trigger.
+---    * Any fields supported by the underlying method call according to `kind`, e.g. `BetterTouchTool:touchbarButton`.
 function obj:bindSpoonActions(spoon, spec)
+  local def = {}
   if spoon and spoon.spoon_action_mappings then
-    local def = spoon.spoon_action_mappings
-    for name,key in pairs(spec) do
-      if def[name] then
-        local code = "spoon."..spoon.name..".spoon_action_mappings['"..name.."']()"
-        obj.logger.df("Generated code for BTT trigger for action %s: %s", name, code)
-        spec[name].code = code
-        if spec[name].kind == 'touchbar_button' then
-          obj.logger.df("Binding TouchBar button according to this spec: %s", hs.inspect(spec[name]))
-          obj:touchbar_button(spec[name].uuid, spec[name])
-        end
-      else
-        obj.logger.ef("Error: BTT binding requested for undefined action '%s'", name)
-      end
+    def = spoon.spoon_action_mappings
+  end
+  for name,key in pairs(spec) do
+    --- Auto-generate code if we have a definition in the spoon and no
+    --- code field is explicitly given in the spec
+    if def[name] and (not spec[name].code) then
+      local code = "spoon."..spoon.name..".spoon_action_mappings[\""..name.."\"]()"
+      obj.logger.df("Generated code for BTT trigger for action %s: %s", name, code)
+      spec[name].code = code
+    end
+    if spec[name].kind == 'touchbarButton' then
+      obj.logger.df("Binding TouchBar button according to this spec: %s", hs.inspect(spec[name]))
+      obj:touchbarButton(spec[name].uuid, spec[name])
+    elseif spec[name].kind == 'touchbarWidget' then
+      obj.logger.df("Binding TouchBar widget according to this spec: %s", hs.inspect(spec[name]))
+      obj:touchbarWidget(spec[name].uuid, spec[name])
     end
   end
 end
